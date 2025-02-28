49be47241b6a34d6dbe1ddbead203f43f62395c3
Miguel Caceres <miguelcaceres.bio@gmail.com>
2025-02-28 13:51:27 -0500

    Actualize el app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+// Variable que almacena la lista de mejores amigos
 
-let listaDeAmigos = [];
+let mejoresAmigos = [];
 
-function name(params) {
-    
+//Función para agregar mejores amigos
+function agregarAmigo() {
+    let amigos = document.getElementById("amigo").value.trim();
+    mejoresAmigos.push(amigos);
+    console.log(mejoresAmigos);
+    document.getElementById("amigo").value = ""; // Limpiar el campo de texto
+    return;
 }
+agregarAmigo()
