e28fa6c1c9a52db9ddbbc7a831520c3de2d7ccf3
Miguel Caceres <miguelcaceres.bio@gmail.com>
2025-03-05 17:29:54 -0500

    Actualice el app.js

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ let mejoresAmigos = [];
 function agregarAmigo() {
     let amigos = document.getElementById("amigo").value.trim();
     // Colocaré doble condición para evitar ingresos vacios o duplicados
-    if (mejoresAmigos.includes(amigos) && amigos !== "") {
+    if (!mejoresAmigos.includes(amigos) && amigos !== "") {
         mejoresAmigos.push(amigos);
         console.log(mejoresAmigos);
-        document.getElementById("amigo").value = ""; // esto para limpiar el especio
+        document.getElementById("amigo").value = ""; // esto lo usaré para limpiar el espacio después de escribir el nombre
     } else if (amigos === "") {
         alert("Por favor, ingresa el nombre de un amigo antes de agregar");
     } else {
         alert(`El amigo llamado ${amigos} ya fue ingresado a la lista, por favor ingresa otro amigo`);
+        document.getElementById("amigo").value = ""; // nuevamente esto lo usaré para limpiar el espacio después de escribir el nombre
     }
 }
 agregarAmigo()
